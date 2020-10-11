bfa70a630edac65334c7b5d8944fb333b68eeef3
Goku-kun <jethvadharmarajsinh@gmail.com>
2020-10-11 12:50:20 +0530

    updated the code to print facts

--- api-experimentation/numbers_are_fun.py
+++ api-experimentation/numbers_are_fun.py
@@ -1,10 +1,7 @@
 import json
 import requests
 
-# number = input('Enter a number: ')
-url = 'http://numbersapi.com/2020'
+number = input('Enter a number: ')
+url = 'http://numbersapi.com/' + number
 
-# while True:
-#     if(requests.get(url).text != "42 is the number of kilometers in a marathon." and ):
-# for i in range(10):
 print(requests.get(url).text)
